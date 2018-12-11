f958109106d473bedb750064fab75a0fa89149b0
Snowy <1309571292@qq.com>
2018-12-11 14:37:03 +0800

    修改内容

--- src/components/IndexComponent/Nav.js
+++ src/components/IndexComponent/Nav.js
@@ -2,26 +2,29 @@ import React from 'react'
 import styles from '../../routes/IndexPage.css'
 
 function Nav(){
-        const paragraphs = ['推荐音乐','热歌榜','搜索']
-        const listItems = paragraphs.map((paragraph,index)=>{
-            return (<li 
-                      key={index} 
-                      className={styles.active} 
-                      onClick={()=>{
-                          this.handleChangeStyle(index)
-                          this.props.handleTabChange(index)
-                        }
-                      } >
-                      {paragraph}
-                    </li>)
-        })
-        return (
-            <nav className={styles.navbar}>
-                <ul className={styles.nav}>
-                    {listItems}              
-                </ul>
-            </nav>
-        ) 
+    function handleChangeStyle(index){
+        
     }
+    const paragraphs = ['推荐音乐','热歌榜','搜索']
+    const listItems = paragraphs.map((paragraph,index)=>{
+        return (<li 
+                    key={index} 
+                    className={styles.active} 
+                    onClick={()=>{
+                        handleChangeStyle(index)
+                        props.handleTabChange(index)
+                    }
+                    } >
+                    {paragraph}
+                </li>)
+    })
+    return (
+        <nav className={styles.navbar}>
+            <ul className={styles.nav}>
+                {listItems}              
+            </ul>
+        </nav>
+    ) 
+}
 
 export default Nav
