6550344fc1404b24cc083953a547fca923cd3713
Snowy <1309571292@qq.com>
2018-12-11 16:23:43 +0800

    接收不同样式名

--- src/components/IndexComponent/Nav.js
+++ src/components/IndexComponent/Nav.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import styles from '../../routes/IndexPage.css'
 
-function Nav(){
-    function handleChangeStyle(index){
-        
-    }
+function Nav({currentIndex,onChangeCurrentIndex}){
     const paragraphs = ['推荐音乐','热歌榜','搜索']
     const listItems = paragraphs.map((paragraph,index)=>{
         return (<li 
                     key={index} 
-                    className={styles.active} 
-                    onClick={()=>{
-                        handleChangeStyle(index)
-                        props.handleTabChange(index)
-                    }
-                    } >
+                     className={currentIndex === index ? `${styles.active}`:`${styles.nonActive}`}
+                     >
                     {paragraph}
                 </li>)
     })
     return (
         <nav className={styles.navbar}>
             <ul className={styles.nav}>
-                {listItems}              
+              {listItems}
             </ul>
         </nav>
     ) 
