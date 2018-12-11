636194381be818ee43af2dc0c2164c72e41846b2
Snowy <1309571292@qq.com>
2018-12-11 17:26:57 +0800

    添加路由

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import SongPage from './routes/SongPage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Route path="/" component={IndexPage} />
+      <Route path="/song" component={SongPage} />
     </Router>
   );
 }
