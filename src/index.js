58c5865e561e10c3e5332678b56dca1354d35631
Snowy <1309571292@qq.com>
2018-12-12 15:55:18 +0800

    引入多个model模块

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dva from 'dva';
 import './index.css';
+import model1 from './models/index'
+import model2 from './models/song'
 
 // 1. Initialize
 const app = dva();
@@ -9,7 +11,8 @@ const app = dva();
 
 // 3. Model
 ///app.model(require('./models/index'));
-app.model(require('./models/index'))
+const models = [model1,model2]
+models.forEach(model=>app.model(model))
 
 // 4. Router
 app.router(require('./router'));
