53808a8fd87845c8673f3022e23de7f0442cf2a6
Snowy <1309571292@qq.com>
2018-12-13 16:15:52 +0800

    初始结构

--- src/components/SongComponent/Lyric.js
+++ src/components/SongComponent/Lyric.js
@@ -1,7 +1,15 @@
 import styles from '../../routes/SongPage.css'
 function Lyric(){
     return (
-        <div>lyric</div>
+        <div className={styles.songInfo}>
+            <h2>爱的重量（电视剧《爱情的开关》主题曲暨片头曲）</h2>
+            <div className={styles.songLyricsContainer}>
+                <div className={styles.songRoll}>
+                    <p className={styles.singer}>作词:王艳薇</p>
+                    <p className={styles.lyric}>失去的瞬间</p>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Lyric
