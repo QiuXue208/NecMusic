dfc83ac82f3be7a7b2bd03803678a18d79d54133
Snowy <1309571292@qq.com>
2018-12-13 18:17:45 +0800

    完善结构

--- src/components/SongComponent/Lyric.js
+++ src/components/SongComponent/Lyric.js
@@ -1,4 +1,5 @@
 import styles from '../../routes/SongPage.css'
+import guideImg from'../../assets/guide.png'
 function Lyric(){
     return (
         <div className={styles.songInfo}>
@@ -8,6 +9,10 @@ function Lyric(){
                     <p className={styles.singer}>作词:王艳薇</p>
                     <p className={styles.lyric}>失去的瞬间</p>
                 </div>
+            </div>
+            <a className={styles.songLink} href="#">查看完整歌词></a>
+            <div className={styles.songGuide}>
+              <img src={guideImg} />
             </div>
         </div>
     )
