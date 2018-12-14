26bfe61b916cdeab20e76aee1b7e5e19878769a2
Snowy <1309571292@qq.com>
2018-12-14 17:52:29 +0800

    添加歌词

--- src/components/SongComponent/Lyric.js
+++ src/components/SongComponent/Lyric.js
@@ -1,31 +1,52 @@
 import styles from '../../routes/SongPage.css'
 import guideImg from'../../assets/guide.png'
+import jsonData from '../../json/song.json'
+// console.log(jsonData)
 
-var p = document.createElement('p')
-p.className = 'lyric'
-p.innerText = '作曲：王艳薇'
-console.log(p)
 
-var xhr = new XMLHttpRequest()
-console.log(xhr)
-xhr.open('GET','../../json/song.json')
-xhr.onreadystatechange = ()=>{
-    if(xhr.readyState === 4 && xhr.status === 200){
-        console.log('success')
-        console.log(xhr.responseText)
-    }
-}
-xhr.send()
+
+
+// var xhr = new XMLHttpRequest()
+// console.log(xhr)
+// xhr.open('GET','../../json/song.json')
+// xhr.onreadystatechange = ()=>{
+//     if(xhr.readyState === 4 && xhr.status === 200){
+//         console.log('success')
+//         console.log(xhr.responseText)
+//     }
+// }
+// xhr.send()
+
+
+// $(function(){
+//     $.getJSON('../../json/song.json',function(response){
+//         console.log(response)
+//     })
+// })
+
+
 
 
 function Lyric(){
+    let array = jsonData.lyric.split('\n')
     return (
         <div className={styles.songInfo}>
-            <h2>爱的重量（电视剧《爱情的开关》主题曲暨片头曲）</h2>
+            <h2>{jsonData.songName}</h2>
             <div className={styles.songLyricsContainer}>
                 <div className={styles.songRoll}>
-                    <p className={styles.lyric}>作词:王艳薇</p>
-                    <p className={styles.lyric}>失去的瞬间</p>
+                 { 
+                     array.map((item,key)=>{
+                        let matches = item.match(/^\[(.*)\](.*)/)
+                        // console.log(matches)
+                     let times = matches[1].match(/^(.*):(.*)/)
+                     console.log(times)
+                     let seconds = times[1]*60 + times[2]
+                     console.log(seconds)
+                        return <p key={key} className={styles.lyric} data-time={matches[1]}>{matches[2]}</p>
+                    })
+                }
+                    {/* <p className={styles.lyric}>作词:王艳薇</p>
+                    <p className={styles.lyric}>失去的瞬间</p> */}
                 </div>
             </div>
             <a className={styles.songLink} href="#">查看完整歌词></a>
