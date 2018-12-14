8acc24f2dcd805953c4d3834c91d4c6302b3bfdb
Snowy <1309571292@qq.com>
2018-12-14 11:03:26 +0800

    bring in img

--- src/components/SongComponent/Disk.js
+++ src/components/SongComponent/Disk.js
@@ -1,6 +1,7 @@
 import styles from '../../routes/SongPage.css'
 import React from 'react'
 import {Icon} from 'antd'
+import backImg from '../../assets/back.png'
 
 const IconFont = Icon.createFromIconfontCN({
     scriptUrl: '//at.alicdn.com/t/font_961967_7e8h3i0rt6.js',
@@ -17,7 +18,7 @@ function Disk (props){
                   <div style={props.value.rollStyle} className={styles.discOver}></div>
                 </div> 
                 <div className={styles.imgContainer}>
-                    <img style={props.value.imgStyle} src="https://p2.music.126.net/kiThDJ2wlbhgX9d3vvVbWw==/109951163709583836.webp?imageView&amp;thumbnail=721y721&amp;quality=75&amp;tostatic=0&amp;type=webp" />
+                    <img style={props.value.imgStyle} src={backImg}/>
                 </div> 
                 <div className={styles.iconContainer} style={props.value.playBtnStyle}>
                     <div className={styles.iconBack}></div>
