d10d3928bcf25edde54999aa6db7cb5dede33e99
Snowy <1309571292@qq.com>
2018-12-14 17:53:33 +0800

    modified

--- src/models/song.js
+++ src/models/song.js
@@ -1,36 +1,31 @@
-var style1 = {display:'block'}
-var style11 = {animationPlayState:'paused'}
-var style2 = {display:'none'}
-var style22 = {animationPlayState:'running'}
+let styleOne = {
+    playBtnStyle:{display:'block'},
+    imgStyle:{animationPlayState:'paused'},
+    rollStyle:{animationPlayState:'paused'}
+}
+let styleTwo = {
+    playBtnStyle:{display:'none'},
+    imgStyle:{animationPlayState:'running'},
+    rollStyle:{animationPlayState:'running'}
+}
 var judgement = false
 var audio = document.createElement('audio')
 audio.src = "http://pjpeuy1al.bkt.clouddn.com/%E7%88%B1%E7%9A%84%E9%87%8D%E9%87%8F.mp3"
 audio.autoplay = true
-audio.onended = ()=>{
-    
-}
+
+let timerId = setInterval(()=>{
+    // console.log(audio.currentTime)
+},500)
 export default{
     namespace:'song',
-    state:{
-        playBtnStyle:style2,
-        imgStyle:style22,
-        rollStyle:style22,
-    },
+    state:styleTwo,
     reducers:{
         change(state){
             judgement = !judgement
             if(judgement){
-                return {
-                        playBtnStyle:style1,
-                        imgStyle:style11,
-                        rollStyle:style11
-                    }
+                return styleOne
             }else{
-                return {
-                        playBtnStyle:style2,
-                        imgStyle:style22,
-                        rollStyle:style22
-                    }
+                return styleTwo
             }
         },
         playOrPause(state){
